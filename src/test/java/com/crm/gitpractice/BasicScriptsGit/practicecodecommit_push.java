40c913cfcb804e7af4f6bc782ef8b6e1725b1c8b
Admin <Admin@Monica>
2021-12-09 13:47:13 +0530

    first commit & push

---
+++ src/test/java/com/crm/gitpractice/BasicScriptsGit/practicecodecommit_push.java
@@ -0,0 +1,13 @@
+package com.crm.gitpractice.BasicScriptsGit;
+
+import org.testng.annotations.Test;
+
+public class practicecodecommit_push {
+	
+	@Test
+	public void practicecodecommit_push() {
+		
+		System.out.println("code for user-1 , script-1");
+	}
+
+}
